f4178aa69367af2aa4eadd784e87029c5825b281
omegaspard <50022079+omegaspard@users.noreply.github.com>
2021-09-12 12:45:22 +0200

    Add countrer before ball starts moving. (#7)

--- Level/Level.gd
+++ Level/Level.gd
@@ -6,11 +6,23 @@ var opponoentPlayerScore = 0
 func _on_ScoringAreaLeft_body_entered(body):
 	$Ball.position = Vector2(640, 360)
 	opponoentPlayerScore += 1
+	get_tree().call_group('BallGroup', 'stop_ball')
+	$CountdownTimer.start()
+	$CountdownTimerLabel.visible = true
 
 func _on_ScoringAreaRight_body_entered(body):
 	$Ball.position = Vector2(640, 360)
 	playerScore += 1
+	get_tree().call_group('BallGroup', 'stop_ball')
+	$CountdownTimer.start()
+	$CountdownTimerLabel.visible = true
 	
 func _process(delta):
 	$PlayerScore.text = str(playerScore)
 	$OpponentPlayerScore.text = str(opponoentPlayerScore)
+	if $CountdownTimerLabel.visible:
+		$CountdownTimerLabel.text = str(int($CountdownTimer.time_left) + 1)
+
+func _on_CountdownTimer_timeout():
+	get_tree().call_group('BallGroup', 'restart_ball')
+	$CountdownTimerLabel.visible = false
