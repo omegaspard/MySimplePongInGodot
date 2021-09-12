123abca9bc632255e7d57d4c08643fd8f14f062e
omegaspard <50022079+omegaspard@users.noreply.github.com>
2021-09-12 12:14:04 +0200

    Add score system. (#6)

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,10 +1,16 @@
 extends Node
 
-
+var playerScore = 0
+var opponoentPlayerScore = 0
 
 func _on_ScoringAreaLeft_body_entered(body):
 	$Ball.position = Vector2(640, 360)
-
+	opponoentPlayerScore += 1
 
 func _on_ScoringAreaRight_body_entered(body):
 	$Ball.position = Vector2(640, 360)
+	playerScore += 1
+	
+func _process(delta):
+	$PlayerScore.text = str(playerScore)
+	$OpponentPlayerScore.text = str(opponoentPlayerScore)
